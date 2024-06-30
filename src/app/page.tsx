863f2a35282d011c5b15bf0e87160de66ec355b3
Dzenis Gudzevic <frontprogramer@gmail.com>
2024-06-30 14:47:34 +0200

    Orbit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,11 @@
 import Headphones from "@/components/Headphones";
 import Experience from "@/components/setup/Experience";
 import Lights from "@/components/setup/Lights";
-import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 
 export default function Home() {
   return (
     <Canvas flat>
-      <OrbitControls />
       <Experience />
       <Lights />
       <Headphones />
