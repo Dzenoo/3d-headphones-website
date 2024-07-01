522c94b02041feb1ed8d9aa01da9eb9e47835920
Dzenis Gudzevic <frontprogramer@gmail.com>
2024-07-01 16:13:23 +0200

    Scroll

--- src/components/Headphones.tsx
+++ src/components/Headphones.tsx
@@ -8,49 +8,59 @@ import { gsap } from "gsap";
 const Headphones: React.FC = () => {
   const { scene } = useGLTF("./models/headphones.glb");
 
-  const ref = useRef<any>();
-  const tl = useRef<any>();
+  const ref = useRef<THREE.Object3D>(null);
+  const tl = useRef<GSAPTimeline | null>(null);
 
   const scroll = useScroll();
 
   useFrame(() => {
-    if (tl) {
-      tl.current.seek(scroll.offset * tl.current!.duration());
+    if (tl.current) {
+      tl.current.seek(scroll.offset * tl.current.duration());
     }
   });
 
   useEffect(() => {
-    tl.current = gsap.timeline();
+    if (ref.current) {
+      tl.current = gsap.timeline();
 
-    tl.current!.to(ref.current!.position, {
-      duration: 0.1,
-      y: -0.1,
-    });
+      tl.current.to(ref.current.position, {
+        duration: 0.1,
+        y: -0.1,
+      });
+
+      tl.current.to(
+        ref.current.position,
+        {
+          duration: 0.5,
+          x: 0.5,
+          z: 0.5,
+        },
+        0
+      );
+    }
   }, []);
 
   useEffect(() => {
-    scene.traverse((child: any) => {
-      if (child.isMesh) {
-        if (child.name === "Circle") {
-          child.material = new THREE.MeshStandardMaterial({
+    scene.traverse((child) => {
+      if ((child as THREE.Mesh).isMesh) {
+        const mesh = child as THREE.Mesh;
+        if (mesh.name === "Circle") {
+          mesh.material = new THREE.MeshStandardMaterial({
             color: new THREE.Color("#000000"),
             roughness: 0.5,
             metalness: 1.0,
           });
         } else if (
-          child.name === "Speaker_Right" ||
-          child.name === "Speaker_Left"
+          mesh.name === "Speaker_Right" ||
+          mesh.name === "Speaker_Left"
         ) {
-          child.material = new THREE.MeshStandardMaterial({
+          mesh.material = new THREE.MeshStandardMaterial({
             color: new THREE.Color("#000000"),
             roughness: 0.5,
             metalness: 0.7,
           });
-        } else if (
-          child.name === "Squis_Right" ||
-          child.name === "Squis_Left"
-        ) {
-          child.material = new THREE.MeshStandardMaterial({
+        } else if (mesh.name === "Squis_Right" || mesh.name === "Squis_Left") {
+          mesh.material = new THREE.MeshStandardMaterial({
             color: new THREE.Color("#000000"),
             roughness: 0.7,
             metalness: 0.9,
@@ -62,7 +72,7 @@ const Headphones: React.FC = () => {
 
   return (
     <Center>
-      <primitive ref={ref} object={scene}></primitive>
+      <primitive ref={ref} object={scene} />
     </Center>
   );
 };
--- src/components/HomeOverlay.tsx
+++ src/components/HomeOverlay.tsx
@@ -6,28 +6,95 @@ const HomeOverlay: React.FC = () => {
   return (
     <Scroll html>
       <div className="px-10 w-screen">
-        <Section>
+        <Section hero>
           <Hero />
         </Section>
-        <Section>
-          <h1>HERO</h1>
+        <Section flex>
+          <div className="flex flex-col gap-5 justify-start ">
+            <div>
+              <h1 className="font-bold text-5xl text-white">
+                Immersive 3D Audio
+              </h1>
+            </div>
+            <div className="max-w-xl">
+              <p className="text-lg text-white">
+                Experience sound like never before with our advanced 3D audio
+                technology. Enjoy a fully immersive auditory experience that
+                places you at the center of your favorite music, movies, and
+                games.
+              </p>
+            </div>
+          </div>
         </Section>
         <Section>
-          <h1>HERO</h1>
+          <div className="flex flex-col gap-5 justify-start">
+            <div>
+              <h1 className="font-bold text-5xl text-white">
+                Noise Cancelling
+              </h1>
+            </div>
+            <div className="max-w-xl">
+              <p className="text-lg text-white">
+                Block out the world and focus on what matters. Our cutting-edge
+                noise cancelling technology ensures that you can enjoy your
+                audio
+              </p>
+            </div>
+          </div>
+        </Section>
+        <Section flex>
+          <div className="flex flex-col gap-5 justify-start">
+            <div>
+              <h1 className="font-bold text-5xl text-white">
+                Wireless Connectivity
+              </h1>
+            </div>
+            <div className="max-w-xl">
+              <p className="text-lg text-white">
+                Say goodbye to tangled wires. With seamless wireless
+                connectivity, our headphones provide the freedom and convenience
+                you need, whether you're on the go or relaxing at home.
+              </p>
+            </div>
+          </div>
         </Section>
         <Section>
-          <h1>HERO</h1>
-        </Section>
-        <Section>
-          <h1>HERO</h1>
+          <div className="flex flex-col gap-5 justify-start">
+            <div>
+              <h1 className="font-bold text-5xl text-white">
+                Comfort-Focused Design
+              </h1>
+            </div>
+            <div className="max-w-xl">
+              <p className="text-lg text-white">
+                Designed with your comfort in mind, our headphones feature plush
+                ear cushions and an ergonomic design that ensures a perfect fit
+                for extended listening sessions.
+              </p>
+            </div>
+          </div>
         </Section>
       </div>
     </Scroll>
   );
 };
 
-const Section = ({ children }: { children: React.ReactNode }) => {
-  return <section className="h-screen w-full">{children}</section>;
+const Section = ({
+  flex,
+  children,
+  hero,
+}: {
+  hero?: boolean;
+  flex?: boolean;
+  children: React.ReactNode;
+}) => {
+  const alig = flex ? "flex flex-col items-end" : "flex flex-col items-start";
+
+  return (
+    <section className={`h-screen w-full ${hero ? null : alig}`}>
+      {children}
+    </section>
+  );
 };
 
 export default HomeOverlay;
